106eb679c6c5cf19a2b9afbd8ca67fac6ea5ec63
Joel Kobjoll <joel@whalar.com>
2024-02-09 17:31:54 +0100

    Update theme provider

--- src/themes/ThemeProvider.tsx
+++ src/themes/ThemeProvider.tsx
@@ -1,11 +1,17 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ThemeOverride, extendTheme } from "@chakra-ui/react";
 
 import { theme } from "./theme";
 
 export interface ICustomThemeProviderProps {
   children: React.ReactNode;
+  theme?: ThemeOverride;
 }
 
 export const ThemeProvider: React.FC<ICustomThemeProviderProps> = ({
   children,
-}) => <ChakraProvider theme={theme}>{children}</ChakraProvider>;
+  theme: customTheme = {},
+}) => (
+  <ChakraProvider theme={extendTheme([theme, customTheme])}>
+    {children}
+  </ChakraProvider>
+);
